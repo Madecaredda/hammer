eb4907b673069ea640be22800863a0de9d642813
Matteo Caredda <madecaredda94@gmail.com>
2016-02-09 19:11:48 +0100

    credenziali

--- Accesso.php
+++ Accesso.php
@@ -1,8 +1,8 @@
 <?php
 class Accesso{
     public static $db_host = 'localhost';
-    public static $db_user= 'root';
-    public static $db_pass= 'davide';
+    public static $db_user= 'careddaMatteo';
+    public static $db_pass= 'scarabeo9443';
     public static $db_nameUsers= 'amm15_careddaMatteo';
 }
 
